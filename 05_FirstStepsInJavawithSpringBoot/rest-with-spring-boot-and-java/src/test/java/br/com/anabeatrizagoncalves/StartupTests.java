d11f2463cfda6a425f51e58bb619e72d0c1387e2
anabeatrizgoncalves <aanabbeatrizzin@gmail.com>
2024-01-04 15:08:25 -0300

    commit incial

---
+++ 05_FirstStepsInJavawithSpringBoot/rest-with-spring-boot-and-java/src/test/java/br/com/anabeatrizagoncalves/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.anabeatrizagoncalves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
